5f019563973ba6bc09c0da65aaaa5b91cb1750ef
Ali Nawab <alinawab9830@gmail.com>
2023-07-05 17:46:15 -0500

    Intialization

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs');
+const db = require('./Develop/db/db.json');
+
+const app = express();
+const PORT = 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(express.static('Develop/public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+);
+
+app.get('/api/notes', (req,res) =>{
+    fs.readFile('Develop/db/db.json', 'utf8', (error, data) => res.json(data))
+});
+
+app.post('/api/notes', (req, res) => {
+    db.push(req.body)
+    fs.writeFile('Develop/db/db.json', JSON.stringify(db), (err) =>
+    err ? res.json(err) : res.json('Success!')
+ ); 
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+});
+
+
